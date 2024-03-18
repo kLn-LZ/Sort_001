7cf8cdfb727de23f378f40f2eac841203b7c9bd9
kLn-LZ <160866564+kLn-LZ@users.noreply.github.com>
2024-03-18 08:36:37 -0300

    Uploading libs

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         ControllerOrdenacao co = new ControllerOrdenacao();
 
-        int[] v = {74, 20, 74, 87, 81, 16, 25, 99, 44, 58};
+        int[] v = {100, 20, 74, 87, 81, 16, 25, 99, 44, 58};
 
         v = co.quickSort(v, 0, v.length - 1);
 
